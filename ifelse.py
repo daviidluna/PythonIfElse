905770a60c9da1267c9231e8d326b76eee4b8c92
David Luna <davidluna@mail.weber.edu>
2021-07-27 22:46:57 -0600

    started with conditionals

---
+++ ifelse.py
@@ -0,0 +1,22 @@
+name = 'david'
+name2 = 'David'
+if name != name2:
+    print('they are not equal')
+
+x = 8
+y = 15
+if x > 15:
+    print('true')
+else:
+    print(False)
+
+z = 90
+y = 89
+x = 78
+if z < x and z < x:
+    print('z is less than')
+elif y > z or x > z:
+    print('both expressions are true')
+elif x == z or x < z:
+    print('at least one expression is true')
+
